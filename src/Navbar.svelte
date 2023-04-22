fdcbd3c52d78aac61dca37f742d3ca64aaa683f3
Simen Woldmo <simen.w.93@hotmail.com>
2023-04-22 05:54:06 +0200

    Added and styled navbar

---
+++ src/Navbar.svelte
@@ -0,0 +1,19 @@
+<div class="navbar">
+  <a href='https://www.decathlon.ch/fr/'><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Decathlon_Logo.svg/1200px-Decathlon_Logo.svg.png?20180901220847" alt="Decathlon Logo" ></a>
+</div>
+
+<style>
+.navbar {
+  width: 100%;
+  /* height: 45px; */
+  background-color: #0C83C3;
+  text-align: center;
+  margin-bottom: 16px;
+}
+
+img {
+  width: 150px;
+  border-bottom: 0.5px solid whitesmoke;
+}
+
+</style>
