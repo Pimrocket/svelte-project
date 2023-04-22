ceb8d4b031b3b2030e74d491d426abbab41da6f3
Simen Woldmo <simen.w.93@hotmail.com>
2023-04-22 06:11:40 +0200

    Added smiley component with minor design

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,27 @@
 <script>
+  import { each } from 'svelte/internal';
   import Navbar from './Navbar.svelte';
-	export let name;
+  import Smiley from './Smiley.svelte';
+	// export let name;
+
+    let smileys = [
+    {
+      src: "./images/verygood.png",
+      smileyType: 'Happy',
+      smileyText: 'Happy',
+      clicked: false
+    }
+  ];
 </script>
 
 <Navbar />
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  {#each smileys as smiley}
+    <Smiley src={smiley.src} alt={smiley.smileyType} smileyText={smiley.smileyText}/>
+  {/each}
 </main>
-
 <style>
 
   :global(body) {
---
+++ src/Smiley.svelte
@@ -0,0 +1,33 @@
+<script>
+
+  // let isClicked = false;
+  // let smileyType = 'Happy';
+  export let smileyType;
+  export let smileyText;
+  export let src;
+  // export let clicked;
+</script>
+
+<style>
+  div {
+    text-align: center;
+  }
+
+  img {
+    width: 65px;
+    opacity: 50%;
+  }
+  img:hover {
+    opacity: 100%;
+  }
+
+  p {
+    margin: 0;
+  }
+</style>
+
+
+<div>
+  <img {src} alt={smileyType} />
+  <p><strong>{smileyText}</strong></p>
+</div>
